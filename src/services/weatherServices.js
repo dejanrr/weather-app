abdf962b024f58649d41c3b1ada8c9892679b895
Dejan Radeljic <radeljic.dejan@gmail.com>
2022-02-14 05:00:22 +0100

    Update weatherServices.js

--- src/services/weatherServices.js
+++ src/services/weatherServices.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
   fetchWeather: (city) => {
-    const url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${city}&days=7`;
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${city}&days=7`;
     return axios.get(url).then((info) => info.data);
   },
 };
